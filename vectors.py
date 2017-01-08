e38234e35ee6658a6187bd6f12a8cc0b5205bd06
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-01-07 18:30:55 -0600

    next

--- vectors.py
+++ vectors.py
@@ -1,5 +1,6 @@
 #Library of common linear algebra vector operations. Used as exercise file for Udacity Linear Algebra Refresher Course 
 
+from math import sqrt 
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -35,8 +36,21 @@ class Vector(object):
         return Vector([c*x for x in self.coordinates])
 
 
-# unit vector: vector with magnitude = 1, represents vector direction 
-# Normalization: find unit vector in same direction as given vector. Scalar multiplication of vector with 1/magnitude(v) 
+    # unit vector: vector with magnitude = 1, represents vector direction 
+    # Normalization: find unit vector in same direction as given vector. Scalar multiplication of vector with 1/magnitude(v) 
+
+
+    def magnitude(self): 
+        coordinates_squared = [x**2 for x in self.coordinates]
+        return sqrt(sum(coordinates_squared))
+
+    def normalize(self): 
+        try: 
+            magnitude = self.magnitude()
+            return self.scalar_product(1.0/magnitude)
+        except: 
+            # if exception occurs in try statement, this runs 
+            raise Exception('Cannot normalize a zero vector!')
 
 
 
@@ -49,3 +63,7 @@ print my_vector3 == my_vector2
 a = Vector([8.218, -9.341])
 b = Vector([-1.129, 2.111])
 print a.sum(b)
+
+v = Vector([1, 2])
+print v.magnitude()
+print v.normalize()
